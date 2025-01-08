6d568e08fd089df76c2d3f88d62a990f2c11e520
AjeetY28 <ajeet.bca28@gmail.com>
2025-01-09 01:15:01 +0530

    adding toast

--- chat-app-fronted/src/App.jsx
+++ chat-app-fronted/src/App.jsx
@@ -2,15 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { toast } from 'react-hot-toast'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
       <h1>This is main Page</h1>
+      <button onClick={()=>{
+        toast.success("this is toast button")
+      }}>Click me</button>
     </div>
-  )
+  );
 }
 
 export default App
