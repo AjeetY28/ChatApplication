2edfaf9dd3e88a124789c76d518b065a7a92f129
AjeetY28 <ajeet.bca28@gmail.com>
2025-01-09 01:48:43 +0530

    create JoinCreateChat page

--- chat-app-fronted/src/App.jsx
+++ chat-app-fronted/src/App.jsx
@@ -3,15 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { toast } from 'react-hot-toast'
+import JoinCreateChat from './components/JoinCreateChat'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <h1>This is main Page</h1>
-      <button onClick={()=>{
-        toast.success("this is toast button")
-      }}>Click me</button>
+      <JoinCreateChat />
     </div>
   );
 }
