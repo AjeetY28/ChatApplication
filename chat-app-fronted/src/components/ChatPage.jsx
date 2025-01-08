456df6d4fdef6b58ba2b22307cd3c6c7706bf341
AjeetY28 <ajeet.bca28@gmail.com>
2025-01-09 03:09:52 +0530

    create input messgae on chatPage

--- chat-app-fronted/src/components/ChatPage.jsx
+++ chat-app-fronted/src/components/ChatPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { MdAttachFile, MdSend } from "react-icons/md";
 
 const ChatPage = () => {
   return (
     <div className=''>
-        <header className='dark:border-gray-700 shadow dark:bg-gray-900 py-5  flex justify-around items-center'>
+
+        {/* header container*/}
+        <header className='dark:border-gray-700 fixed w-full shadow dark:bg-gray-900 py-5  flex justify-around items-center'>
             {/* room name container*/}
             <div className=''>
                 <h1 className="text-xl font-semibold">
@@ -24,6 +27,26 @@ const ChatPage = () => {
                 </button>
             </div>
         </header>
+
+        {/* Input message container*/}
+
+        <div className=' fixed bottom-4 w-full h-16'>
+           <div className='h-full pr-10  gap-4  flex items-center justify-between rounded-full w-1/2 mx-auto  dark:bg-gray-900'>
+                <input type="text" className='dark:border-gray-600 w-full  dark:bg-gray-800 w h-full rounded-full px-5 py-2 focus:outline-none' 
+                placeholder='Enter your message here'/>
+               
+               <div className='flex gap-1'>
+                    <button className='dark:bg-purple-600 px-3 py-2 h-10 w-10 flex justify-center items-center rounded-full'>
+                    <MdAttachFile size={20} />
+                    </button>
+
+                    <button className='dark:bg-green-600 px-3 py-2 h-10 w-10 flex justify-center items-center rounded-full'>
+                    <MdSend size={20} />
+                    </button>
+               </div>
+           </div>
+        </div>
+
     </div>
   )
 };
