056700b6bab99b005c07bddeaa06c89513c7e5be
AjeetY28 <ajeet.bca28@gmail.com>
2025-02-14 00:57:52 +0530

    added

--- chat-app-fronted/src/config/AxiosHelper.js
+++ chat-app-fronted/src/config/AxiosHelper.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const baseURL = "http://localhost:8080";
+export const baseURL = "http://43.205.9.233:8080";
 export const httpClient = axios.create({
     baseURL: baseURL,
     });
